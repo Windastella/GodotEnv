1160acfc0691051b0cf53e64ce2063fcda8e6827
Jonathan Eeckhout <eeckhout.jonathan@gmail.com>
2023-07-07 19:42:24 +0200

    simplify code

--- README.md
+++ README.md
@@ -6,10 +6,6 @@ A simple implementation of DotEnv. Allow usage of .env file in godot. Also retri
 
 Add `/godotenv/scripts/env.gd` to Autoloader.
 
-Retrieve Environment Variable by
-
-`var env_var = get_node("/root/env").get_value("ENV_VAR")`
-
-Or if you left the default name of the Autoloader
+If you left the default name of you script in the Autoloader you can retrieve the Environment Variable by
 
 `Env.get_value("ENV_VAR")`
--- godotenv/scripts/env.gd
+++ godotenv/scripts/env.gd
@@ -3,22 +3,35 @@
 # Email: nik96mirza[at]gmail.com
 extends Node
 
-var env = {};
-
-@onready var parser = GodotEnvParser.new();
-
-
-func _ready():
-	env = parser.parse("res://.env");
-
 
 func get_value(valuename: String):
 	# prioritized os environment variable
-	if(OS.has_environment(valuename)):
-		return OS.get_environment(valuename);
+	var env = parse("res://.env")
 
-	if(env.has(valuename)):
-		return env[valuename];
+	if OS.has_environment(valuename):
+		return OS.get_environment(valuename)
+
+	if env.has(valuename):
+		return env[valuename]
 
 	# return empty
-	return "";
+	return ""
+
+
+func parse(filename):
+	if !FileAccess.file_exists(filename):
+		return {}
+
+	var file = FileAccess.open(filename, FileAccess.READ)
+
+	var env = {}
+	var line = ""
+
+	while !file.eof_reached():
+		line = file.get_line()
+		var o = line.split("=")
+
+		if o.size() == 2:  # only check valid lines
+			env[o[0]] = o[1].lstrip('"').rstrip('"')
+
+	return env
